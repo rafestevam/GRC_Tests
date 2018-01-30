1dec6be9c7cdc0bd18ae90f27a5ceefe2f68a6ad
Rafael Estevam <rafael.estevam@exedconsulting.com>
2018-01-30 13:58:10 -0200

    EV131567 - Criacao da classe de testes de listagem de apontamentos

--- src/com/exed/testes/auditoria/TestarListagemDeApontamentos.java
+++ src/com/exed/testes/auditoria/TestarListagemDeApontamentos.java
@@ -1,12 +1,36 @@
 package com.exed.testes.auditoria;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestarListagemDeApontamentos {
 	
 	@Test
 	public void acessarListagem(){
 		
+		//FirefoxDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:/ChromeDriver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("http://localhost:8080/arcm/login.jsp");
+		
+		this.logon("f000580", "manager", driver);
+		
+	}
+	
+	private void logon(String userName, String pass, WebDriver driver){
+		
+		WebElement name = driver.findElement(By.id("username"));
+		name.sendKeys(userName);
+		
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys(pass);
+		
+		name.submit();
+
 	}
 
 }
